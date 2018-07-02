29d7d30b4452e579c863652b32959661f245ebe2
nsheff <nsheff@users.noreply.github.com>
2018-07-02 11:33:58 -0400

    add empty template setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='refgenie',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/databio/refgenie',
+    author='Nathan Sheffield',
+    author_email='nathan@code.databio.org',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
