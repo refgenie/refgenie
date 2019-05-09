bbe3de80e219099b1e5d156157845f39d15acea1
nsheff <nsheff@users.noreply.github.com>
2019-05-09 16:32:25 -0400

    Docs, setup

---
+++ docs/download.md
@@ -0,0 +1,16 @@
+# Download pre-built indexed reference genomes
+
+These are built indexes for common genomes. These pre-built downloads are `tar gzipped` files, so **you will need to unarchive them after downloading**. The complete collection is listed at [http://big.databio.org/refgenomes/](http://big.databio.org/refgenomes/):
+
+### Mirror 1:
+
+* Human: [hg38.tgz](http://big.databio.org/refgenomes/hg38.tgz), [hg19.tgz](http://big.databio.org/refgenomes/hg19.tgz)
+* Rat: [rn6.tgz](http://big.databio.org/refgenomes/rn6.tgz)
+* Mouse: [mm10](http://big.databio.org/refgenomes/mm10.tgz)
+* Prealignment 'decoy' references: available at [big.databio.org/refgenomes/](http://big.databio.org/refgenomes/) for sequences from [ref_decoy](https://github.com/databio/ref_decoy))
+
+### Mirror 2 (use if mirror 1 is down):
+
+* Human: [hg38.tgz](http://cloud.databio.org/refgenomes/hg38.tgz), [hg19.tgz](http://cloud.databio.org/refgenomes/hg19.tgz)
+* Rat: [rn6.tgz](http://cloud.databio.org/refgenomes/rn6.tgz)
+* Mouse: mm9 (pending), [mm10](http://cloud.databio.org/refgenomes/mm10.tgz)
--- setup.py
+++ setup.py
@@ -1,13 +1,50 @@
 from setuptools import setup
 
+
+# Ordinary dependencies
+DEPENDENCIES = []
+with open("requirements/requirements-all.txt", "r") as reqs_file:
+    for line in reqs_file:
+        if not line.strip():
+            continue
+        #DEPENDENCIES.append(line.split("=")[0].rstrip("<>"))
+        DEPENDENCIES.append(line)
+
+extra["install_requires"] = DEPENDENCIES
+
+with open("looper/_version.py", 'r') as versionfile:
+    version = versionfile.readline().split()[-1].strip("\"'\n")
+
+# Handle the pypi README formatting.
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError, OSError):
+    long_description = open('README.md').read()
+
 setup(
     name='refgenie',
-    version='0.0.1',
-    description='',
-    long_description='',
+    packages=["refgenie"],
+    version=version,
+    description='A standardized reference genome indexer',
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ],    
+    license="BSD2",
+    entry_points={
+        "console_scripts": [
+            'looper = looper.looper:main'
+        ],
+    },
+    keywords="bioinformatics, sequencing, ngs",
     url='https://github.com/databio/refgenie',
     author='Nathan Sheffield',
     author_email='nathan@code.databio.org',
-    packages=[],
-    classifiers=['Development Status :: 1 - Planning'],
+    **extra
 )
