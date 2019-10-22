d4733daab599c86d8a5db4264f3baa8ea2edfdea
Jason Smith <jps3dp@virginia.edu>
2019-10-22 10:43:41 -0400

    update messaging for servers with no assets available to list

--- refgenie/refgenie.py
+++ refgenie/refgenie.py
@@ -703,9 +703,9 @@ def main():
                     _LOGGER.info("{} assets:\n{}\n".format(pfx, assets))
                 except DownloadJsonError:
                     bad_servers.append(server_url)
-                    _LOGGER.warning("Could not list assets from {}".format(server_url))  # JPS DEBUG
+                    #_LOGGER.warning("Could not list assets from {}".format(server_url))  # JPS DEBUG
                     if num_servers == len(server_list):
-                        _LOGGER.error("Could not list assets from any of the following server(s), {}".format(bad_servers))  # JPS DEBUG
+                        _LOGGER.error("Could not list assets from the following server(s), {}".format(bad_servers))  # JPS DEBUG
                     else:
                         continue
             rgc.genome_server = server_list
